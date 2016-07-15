44398a24089d00324da5bf488d01c63e69da97d9
Jhonny B <jhonnybsg.dev@gmail.com>
2016-07-15 00:17:38 -0400

    Music Player added

--- app/src/main/java/ufam/edu/com/br/apollo/view/MainActivity.java
+++ app/src/main/java/ufam/edu/com/br/apollo/view/MainActivity.java
@@ -1,9 +1,13 @@
 package ufam.edu.com.br.apollo.view;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -24,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnChooseFile;
     private Button btnShareFile;
     private TextView tvFileName;
-
+    private String filePath;
 
 
     @Override
@@ -45,6 +49,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnShareFile.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MusicPlayerScreen.class);
+                intent.putExtra("audioFileName", filePath);
+                startActivity(intent);
+
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("Permission", "Granted");
+                } else {
+                    Log.e("Permission", "Denied");
+                }
+                return;
+            }
+        }
     }
 
     private void showChooser() {
@@ -74,10 +113,11 @@ public class MainActivity extends AppCompatActivity {
                             // Get the file path from the URI
 
                             final String path = FileUtils.getPath(this, uri);
-                            if (!FileUtils.getExtension(path).equals(".mp3")){
+                            filePath = path;
+                            if (!FileUtils.getExtension(path).equals(".mp3")) {
                                 Toast.makeText(MainActivity.this,
-                                    "You must choose a mp3 file, bitch.", Toast.LENGTH_LONG).show();
-                            }else {
+                                        "You must choose a mp3 file, bitch.", Toast.LENGTH_LONG).show();
+                            } else {
                                 btnShareFile.setVisibility(View.VISIBLE);
                                 tvFileName.setText(FileUtils.getFileName(path));
                             }
